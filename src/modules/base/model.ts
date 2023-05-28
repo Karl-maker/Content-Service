394d6c1872d07fd24811dac7e36a7b7e4f19874f
Karl-Johan Bailey <karl@cenedex.com>
2023-05-28 04:30:55 -0400

    feat(base module): added base model for all to come after

---
+++ src/modules/base/model.ts
@@ -0,0 +1,31 @@
+import mongoose, { Model, Document, Query } from 'mongoose';
+
+class BaseModel<T extends Document> {
+  public model: Model<T>;
+
+  constructor(schema: mongoose.Schema) {
+    this.model = mongoose.model<T>(this.constructor.name, schema);
+  }
+
+  async create(data: Partial<T>): Promise<T> {
+    return await this.model.create(data);
+  }
+
+  async findById(id: string): Promise<T | null> {
+    return await this.model.findById(id).exec();
+  }
+
+  async find(query: object): Promise<T[]> {
+    return await this.model.find(query).exec();
+  }
+
+  async deleteById(_id: string): Promise<any> {
+    return await this.model.deleteOne({ _id }).exec();
+  }
+
+  // Add other common methods as needed
+
+  // You can also add instance methods and static methods specific to each model
+}
+
+export default BaseModel;
